e552d2fc0df54167a04e19c2f4bdb6ef4cc74d77
Ricardo Silva <ricardoatsilva1@gmail.com>
2020-04-08 15:39:04 -0300

    Creating launcher activity

---
+++ app/src/main/java/br/com/tembici/architecture/usage/userPlans/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.tembici.architecture.usage.userPlans
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.tembici.architecture.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
